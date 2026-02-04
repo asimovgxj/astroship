0903004e3fe2e2eb559e700ff4d2734cee69ef30
asimovgxj <asimovgxj@users.noreply.github.com>
2026-02-04 17:37:52 +0800

    ci: add workflow and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,14 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
-// https://astro.build/config
+const repo = "astroship";
+const target = process.env.DEPLOY_TARGET;
+const isGithubPages = target === "GITHUB_PAGES";
+
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: isGithubPages ? `https://asimovgxj.github.io/${repo}` : undefined,
+  base: isGithubPages ? `/${repo}` : "/",
+  outDir: "dist",
   integrations: [mdx(), sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],
