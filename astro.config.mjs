7e1dbe34389905688661a56a52b5a5d517864f05
asimovgxj <75306406+asimovgxj@users.noreply.github.com>
2026-02-23 22:31:03 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
-const repo = "astroship";
-const target = process.env.DEPLOY_TARGET;
-const isGithubPages = target === "GITHUB_PAGES";
-
 export default defineConfig({
-site: isGithubPages ? `https://asimovgxj.github.io/${repo}` : 'http://localhost:4321',
- 
-  base: isGithubPages ? `/${repo}` : "/",
+  // 核心：必须是 https 协议
+  site: 'https://autochina.org',
+  base: "/",
   outDir: "dist",
   integrations: [mdx(), sitemap(), icon()],
   vite: {
